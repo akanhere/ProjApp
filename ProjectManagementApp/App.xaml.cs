2d05ef9d7f56fb20d528eceb881ae930a5bc99c7
ashka <ashka@HOME-PC>
2024-09-03 09:00:41 +0530

    Add project files.

---
+++ ProjectManagementApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectManagementApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
