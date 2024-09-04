90d87e88f870b663b1e8285e4c9b21d659ac5a84
ashwin.kanhere <ashwin.kanhere@gmail.com>
2024-09-04 20:34:27 +0530

    initial commit

--- ProjectManagementApp/MauiProgram.cs
+++ ProjectManagementApp/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProjectManagementApp.Data;
+using System;
 
 namespace ProjectManagementApp
 {
@@ -9,14 +13,19 @@ namespace ProjectManagementApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite("Filename=ProjAppData.db");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ ProjectManagementApp/Services/Navigation/NavigationService.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp.Services.Navigation
+{
+    public class MauiNavigationService : INavigationService
+    {
+
+        public MauiNavigationService()
+        {
+
+        }
+
+        public async Task InitializeAsync()
+        {
+            //var user = await _appEnvironmentService.IdentityService.GetUserInfoAsync();
+
+            await NavigateToAsync("//Home");
+        }
+
+        public Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null)
+        {
+            var shellNavigation = new ShellNavigationState(route);
+
+            return routeParameters != null
+                ? Shell.Current.GoToAsync(shellNavigation, routeParameters)
+                : Shell.Current.GoToAsync(shellNavigation);
+        }
+
+        public Task PopAsync()
+        {
+            return Shell.Current.GoToAsync("..");
+        }
+    }
+}
